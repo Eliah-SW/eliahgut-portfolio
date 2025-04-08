433845b26c7756e37e5c5d788f289e4eeffafb9b
Eliah Gut <eliah.gut@swisscom.com>
2025-04-08 14:57:47 +0200

    car-dealer added

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const translations = {
     de: {
         heroText: "Eliah Gut",
         heroSubText: "Informatiker | Applikationsentwickler",
-        aboutMe: "Ich bin im 1. Lehrjahr als Informatiker Applikationsentwickler. Ich wohne im Kanton Freiburg und habe meine Lehre gerade begonnen. Ich setze gerne meine Ideen in Projekte um.",
+        aboutMe: "Ich bin im 1. Lehrjahr als Informatiker Applikationsentwickler und wohne im Kanton Freiburg. Meine Lehre habe ich gerade begonnen, und ich freue mich darauf, mein Wissen und meine Fähigkeiten in der Praxis zu vertiefen. Ich setze gerne meine Ideen in Projekte um und habe bereits einige kleine Projekte gemacht. Besonders interessieren mich Webentwicklung, Datenbanken und APIs.",
         aboutTitle: "Über mich",
         frontendTitle: "Frontend",
         backendTitle: "Backend",
@@ -13,6 +13,7 @@ const translations = {
         projectF1: "F1 Angular Webseite",
         projectLocoGeo: "Loco Geo",
         projectFerrariAPI: "Ferrari Rest API",
+        projectAutohaendler: "Auto Händler",
         tennis: "Tennis",
         hockey: "Eishockey",
         ski: "Ski",
@@ -22,6 +23,7 @@ const translations = {
         f1Text: "Eine Webseite, die mit Angular erstellt wurde und sich mit einer F1 API verbindet. Sie zeigt alle Fahrer oder Strecken an, mit einer Detailansicht für jeden.",
         locoGeoText: "Eine Angular-Webseite, auf der du ein Flaggenquiz machen kannst. Am Ende wird eine Punktzahl mit der Anzahl der richtigen Antworten und der benötigten Zeit angezeigt.",
         ferrariAPI: "Eine REST API, die mit Java Spring Boot erstellt wurde und verschiedene Ferrari-Modelle enthält. Sie umfasst mehrere Endpunkte, an denen Ferraris sortiert und gefiltert werden können.",
+        autohaendler: "Ein grosses MySQL-Projekt, bei dem wir für einen Autohändler eine Datenbank erstellt haben, mit der er alles von Mitarbeitern bis hin zu Wartungen einsehen kann.",
         moreAboutMe: "Mehr über mich",
         contactMe: "Kontakt aufnehmen",
         phone: "Telefon",
@@ -31,12 +33,13 @@ const translations = {
     en: {
         heroText: "Eliah Gut",
         heroSubText: "Computer Scientist | Application Developer",
-        aboutMe: "I am in the 1st year as a computer scientist application developer. I live in the canton of Fribourg and have just started my apprenticeship. I love implementing my ideas into projects.",
+        aboutMe: "I am in my first year as an application developer apprentice and live in the canton of Fribourg. I have just started my apprenticeship and am excited to deepen my knowledge and skills in practice. I enjoy turning my ideas into projects and have already worked on a few small projects. I am particularly interested in web development, databases, and APIs.",
         aboutTitle: "About Me",
         frontendTitle: "Frontend",
         backendTitle: "Backend",
         otherSkillsTitle: "Other Skills",
         projectsTitle: "Projects",
+        projectAutohaendler: "Car Dealer",
         hobbiesTitle: "My Hobbies",
         contactTitle: "Contact",
         projectF1: "F1 Angular Website",
@@ -51,6 +54,7 @@ const translations = {
         f1Text: "A website built with Angular that connects to an F1 API. It displays all drivers or tracks, with a detailed view for each.",
         locoGeoText: "An Angular website where you can take a flag quiz. At the end, it shows a score with the number of correct answers and the time taken.",
         ferrariAPI: "A REST API built with Java Spring Boot, featuring various Ferrari models. It includes several endpoints where Ferraris can be sorted and filtered.",
+        autohaendler: "A big MySQL project where we created a database for a car dealer, so they can see everything from employees to maintenance.",
         moreAboutMe: "More About Me",
         contactMe: "Get in Touch",
         phone: "Phone",
@@ -76,10 +80,12 @@ function switchLanguage(language) {
 
     document.querySelector(".card:nth-child(1) h3").textContent = translations[language].projectF1;
     document.querySelector(".card:nth-child(1) p").textContent = translations[language].f1Text;
-    document.querySelector(".card:nth-child(2) h3").textContent = translations[language].projectLocoGeo;
-    document.querySelector(".card:nth-child(2) p").textContent = translations[language].locoGeoText;
-    document.querySelector(".card:nth-child(3) h3").textContent = translations[language].projectFerrariAPI;
-    document.querySelector(".card:nth-child(3) p").textContent = translations[language].ferrariAPI;
+    document.querySelector(".card:nth-child(3) h3").textContent = translations[language].projectLocoGeo;
+    document.querySelector(".card:nth-child(3) p").textContent = translations[language].locoGeoText;
+    document.querySelector(".card:nth-child(2) h3").textContent = translations[language].projectFerrariAPI;
+    document.querySelector(".card:nth-child(2) p").textContent = translations[language].ferrariAPI;
+    document.querySelector(".card:nth-child(4) h3").textContent = translations[language].projectAutohaendler;
+    document.querySelector(".card:nth-child(4) p").textContent = translations[language].autohaendler;
 
     document.querySelector(".hobby:nth-child(1) h3").textContent = translations[language].tennis;
     document.querySelector(".hobby:nth-child(2) h3").textContent = translations[language].hockey;
